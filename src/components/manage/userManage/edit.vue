e508035422b3bc70422ff653f52587418bb30e7f
wangfang <18390239389@163.com>
2018-01-04 15:10:02 +0800

    提交

--- src/components/manage/userManage/edit.vue
+++ src/components/manage/userManage/edit.vue
@@ -96,7 +96,7 @@
                         password:this.formCustom.passwd,//登陆密码
                         isDisabled:this.status,//用户状态 0 启用 1 禁用
                     }
-                    let url = common.path2+'system/api/baseUser/';
+                    let url = common.path2+'baseUsers/update';
                     console.log(ajaxData)
                     this.$http.put(
                         url,
